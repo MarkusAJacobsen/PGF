52f9fff417836951a45ffc73193bb9c7103fbc14
Horcon <maciejp@stud.ntnu.no>
2019-03-19 18:24:52 +0100

    PGF-3 #time 1h #comment Bare-bones functionality to log in with google.

--- App.js
+++ App.js
@@ -9,6 +9,9 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+
+GoogleSignin.configure();
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,6 +28,11 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <GoogleSigninButton
+    style={{ width: 192, height: 48 }}
+    size={GoogleSigninButton.Size.Wide}
+    color={GoogleSigninButton.Color.Dark}
+    onPress={_signIn} />
       </View>
     );
   }
@@ -48,3 +56,22 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+// Somewhere in your code
+_signIn = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const userInfo = await GoogleSignin.signIn();
+    this.setState({ userInfo });
+  } catch (error) {
+    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      // user cancelled the login flow
+    } else if (error.code === statusCodes.IN_PROGRESS) {
+      // operation (f.e. sign in) is in progress already
+    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      // play services not available or outdated
+    } else {
+      // some other error happened
+    }
+  }
+};
