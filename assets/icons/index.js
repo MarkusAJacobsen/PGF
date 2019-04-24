320edd06773c3ae1d09d1ebaa53fdce54fed75c8
Dovydas Sileika <dovydas.sileika@evry.com>
2019-04-24 09:40:21 +0200

    added assets

---
+++ assets/icons/index.js
@@ -0,0 +1,10 @@
+const images = {
+  vegetables: require('./vegetables.png'),
+  flowers: require('./flowers.png'),
+  fruits: require('./fruits.png'),
+  herbs: require('./herbs.png'),
+  plus: require('./plus.png'),
+  checked: require('./checked.png'),
+};
+
+export default images;
